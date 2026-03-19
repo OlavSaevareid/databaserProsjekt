9299df72798ff96b5badcb47a66c30a5c6d721cf
Olav <olav.o.saevareid@gmail.com>
2026-03-19 16:33:01 +0100

    fjernet script her da denne er ren sql-spørring

--- python/usecase1.py
+++ python/usecase1.py
@@ -2,20 +2,7 @@ import sqlite3
 import os
 
 def usecase1():
-    baseDir = os.path.dirname(__file__) #Current directory-->Python
-    dbPath = os.path.join(baseDir, "..", "treningDB.db")
-    usecasePath = os.path.join(baseDir, "..", "sql", "usecase1.sql")
-
-    connection = sqlite3.connect(dbPath)
-    cursor = connection.cursor()
-
-    with open(usecasePath,"r", encoding="Utf-8") as queryRaw:
-        query = queryRaw.read()
-
-    cursor.execute(query)
-    connection.commit()
-    print("query utført")
-    connection.close()
+    return "Denne er ren SQL, of kan finnes i sql-mappa"
 
 if __name__ == "__main__":
     usecase1()
