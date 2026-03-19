c014c650d47bb15621b93c829fef8298d8763cb9
Olav <olav.o.saevareid@gmail.com>
2026-03-19 16:37:57 +0100

    Fikset print

--- python/usecase1.py
+++ python/usecase1.py
@@ -2,7 +2,8 @@ import sqlite3
 import os
 
 def usecase1():
-    return "Denne er ren SQL, of kan finnes i sql-mappa"
+    print("Denne er ren SQL, of kan finnes i sql-mappa")
 
 if __name__ == "__main__":
     usecase1()
+    
