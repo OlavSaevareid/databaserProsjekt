91f2e96f1957b91a2d6bd9f13cbb09d8da9c47f5
Olav <olav.o.saevareid@gmail.com>
2026-03-19 16:40:50 +0100

    lagt til litt kode. må fikse :starttid som ikke skal være med

---
+++ python/usecase3.py
@@ -0,0 +1,24 @@
+import sqlite3
+import os
+
+def usecase3(epost, aktivitetstype):
+    baseDir = os.path.dirname(__file__) #Current directory-->Python
+    dbPath = os.path.join(baseDir, "..", "treningDB.db")
+    usecasePath = os.path.join(baseDir, "..", "sql", "usecase3.sql")
+
+    connection = sqlite3.connect(dbPath)
+    cursor = connection.cursor()
+
+    with open(usecasePath,"r", encoding="Utf-8") as queryRaw:
+        query = queryRaw.read()
+
+    cursor.execute(query, {
+        "epost": epost,
+        "aktivitetstype" : aktivitetstype
+    })
+    connection.commit()
+    print("query utført")
+    connection.close()
+
+if __name__ == "__main__":
+    usecase3()
