4ea0e5d7c226aafeb1f3e916fc59a9bd00772472
Headshotsplz <markusandresen11@gmail.com>
2026-03-20 10:45:30 +0100

    Update usecase4.sql

--- sql/usecase4.sql
+++ sql/usecase4.sql
@@ -1,4 +1,10 @@
-SELECT DISTINCT g.StartTid, g.SluttTid, s.Navn AS Senter, sal.Navn AS Sal, a.Beskrivelse AS Aktivitet, i.Fornavn AS Instruktor
+SELECT DISTINCT g.StartTid, 
+                g.SluttTid,
+                s.Navn AS Senter,
+                sal.Navn AS Sal,
+                a.Beskrivelse AS Aktivitet,
+                i.Fornavn AS Instruktor
+
 FROM Gruppetime AS g 
 JOIN Aktivitetstype AS a 
     ON g.AktivitetstypeID = a.AktivitetstypeID
@@ -8,6 +14,7 @@ JOIN Senter AS s
     ON s.SenterID = sal.SenterID
 JOIN Instruktor AS i 
     ON i.InstruktorID = g.InstruktorID
+
 WHERE g.StartTid >= :startdato
-    AND g.SluttTid < date(:startdato, '+7 day')
+    AND g.StartTid < date(:startdato, '+7 day')
 ORDER BY g.StartTid;
