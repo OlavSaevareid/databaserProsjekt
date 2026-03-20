5d2ade924fc9cd2e3b960b2456c13c35c41550bd
Olav <olav.o.saevareid@gmail.com>
2026-03-20 10:19:16 +0100

    fikset skrivefeil

--- python/usecase1.py
+++ python/usecase1.py
@@ -1,5 +1,5 @@
 def usecase1():
-    print("Denne er ren SQL, of kan finnes i sql-mappa")
+    print("Denne er ren SQL, og kan finnes i sql-mappa ved å kjøre 'cd ../sql'")
 
 if __name__ == "__main__":
     usecase1()
