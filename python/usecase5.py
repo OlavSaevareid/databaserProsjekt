5fce1693292ae009572f6ba1716eafbdbf2c26ff
Headshotsplz <markusandresen11@gmail.com>
2026-03-20 11:10:55 +0100

    Update usecase5.py

---
+++ python/usecase5.py
@@ -0,0 +1,17 @@
+import sqlite3
+from pathlib import Path
+
+def usecase5(aktivitetstype, senter, startdato, starttid)
+    baseDir = Path(__file__).resolve().parent
+    dbPath = baseDir.parent / "treningDB.db"
+    usecasePath = baseDir.parent / "sql" / "usecase5.sql"
+        
+    connection = sqlite3.connect(dbPath)
+    cursor = connection.cursor()
+    
+        with open(usecasePath,"r", encoding="Utf-8") as queryRaw:
+        query = queryRaw.read()
+        
+         cursor.execute(query, {
+        "startdato": startdato
+    })
