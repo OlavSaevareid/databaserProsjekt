3080cb5456edf8123e5896c35ea7567f86e1653f
Olav <olav.o.saevareid@gmail.com>
2026-03-21 15:24:36 +0100

    Henter først antall prikker. Deretter oppdater UtestengtTil

--- sql/usecase6.sql
+++ sql/usecase6.sql
@@ -1,21 +1,22 @@
---svartelisting
 WITH Prikkstatus AS (
-    SELECT u.BrukerID, COUNT(*) AS AntallPrikker, MIN(p.Tid) AS ForstePrikk
+    SELECT 
+        u.BrukerID,
+        COUNT(*) AS AntallPrikker,
+        MIN(p.Tid) AS ForstePrikk
     FROM Bruker AS u
-    JOIN Prikk AS p 
-        ON u.BrukerID = p.BrukerID
+    JOIN Prikk AS p
+        ON p.BrukerID = u.BrukerID
     WHERE u.Epost = :epost
-      AND p.Tid >= date(:referansedato, '-30 day')
-      AND p.Tid <= date(:referansedato)
+      AND p.Tid BETWEEN DATETIME(:referansetid, '-30 days') AND DATETIME(:referansetid)
     GROUP BY u.BrukerID
 )
-UPDATE Bruker AS u
+UPDATE Bruker
 SET UtestengtTil = (
-    SELECT date(ForstePrikk, '+30 day')
+    SELECT DATETIME(ForstePrikk, '+30 days')
     FROM Prikkstatus
-    WHERE Prikkstatus.BrukerID = u.BrukerID
+    WHERE Prikkstatus.BrukerID = Bruker.BrukerID
 )
-WHERE u.BrukerID IN (
+WHERE BrukerID IN (
     SELECT BrukerID
     FROM Prikkstatus
     WHERE AntallPrikker >= 3
