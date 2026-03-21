df1e44fe512ef8ce4ea4817c55708c2ccb7f36e1
Olav <olav.o.saevareid@gmail.com>
2026-03-21 15:49:13 +0100

    Se forrige commit

--- sql/usecase8.sql
+++ sql/usecase8.sql
@@ -8,7 +8,7 @@ JOIN Bruker AS u1
     ON b1.BrukerID = u1.BrukerID
 JOIN Bruker AS u2 
     ON b2.BrukerID = u2.BrukerID
-WHERE b1.Status = 'mott'
-  AND b2.Status = 'mott'
+WHERE (b1.Status = 'BOOKET' AND b1.OppmoteTid IS NOT NULL)
+  AND (b2.Status = 'BOOKET' AND b2.OppmoteTid IS NOT NULL)
 GROUP BY u1.Epost, u2.Epost
 ORDER BY antallFelles DESC;
