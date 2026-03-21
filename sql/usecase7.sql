37a9df66fce890bc54ea3c996ae42684ea175055
Olav <olav.o.saevareid@gmail.com>
2026-03-21 15:39:50 +0100

    Endret mott til BOOKET og sjekk IS NOT NULL på oppmotetid

--- sql/usecase7.sql
+++ sql/usecase7.sql
@@ -5,7 +5,8 @@ JOIN Bruker AS u
     ON u.BrukerID = b.BrukerID
 JOIN Gruppetime AS g 
     ON g.GruppetimeID = b.GruppetimeID
-WHERE b.Status = 'mott'
+WHERE b.Status = 'BOOKET'
+AND b.OppmoteTid IS NOT NULL
 AND strftime('%m', g.StartTid) = :maaned
 GROUP BY b.BrukerID, u.Fornavn, u.Etternavn
 HAVING COUNT(*) = (
@@ -15,7 +16,8 @@ HAVING COUNT(*) = (
         FROM Booking AS b2
         JOIN Gruppetime AS g2 
             ON b2.GruppetimeID = g2.GruppetimeID
-        WHERE b2.Status = 'mott'
+        WHERE b2.Status = 'BOOKET'
+        AND b2.OppmoteTid IS NOT NULL
         AND strftime('%m', g2.StartTid) = :maaned
         GROUP BY b2.BrukerID
     )
