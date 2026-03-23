d60b8b65f817ccdcd4370262d5365b0de5b41597
Olav <olav.o.saevareid@gmail.com>
2026-03-23 11:23:19 +0100

    ResetDB fjerner nå all data. CreateDB lager bare skjema

--- python/createDB.py
+++ python/createDB.py
@@ -5,23 +5,23 @@ def createDB():
     baseDir = os.path.dirname(__file__) #Current directory-->Python
     dbPath = os.path.join(baseDir, "..", "treningDB.db")
     schemaPath = os.path.join(baseDir, "..", "sql", "schema.sql")
-    dataPath = os.path.join(baseDir, "..", "sql", "usecase1.sql")
 
     connection = sqlite3.connect(dbPath)
     cursor = connection.cursor()
 
     print("Kobler til/lager databasen")
+    try:
+        with open(schemaPath, "r", encoding = "utf-8") as schemaRaw:
+            schema = schemaRaw.read()
+            cursor.executescript(schema)
 
-    with open(schemaPath, "r", encoding = "utf-8") as schemaRaw:
-        schema = schemaRaw.read()
-        cursor.executescript(schema)
+        print("Oppretter skjema")
 
-    print("Oppretter skjema")
+        connection.commit()
 
-    connection.commit()
-    connection.close()
-
-    print("Databasen er opprettet med skjema og data")
+        print("Databasen er opprettet med skjema")
+    finally: 
+        connection.close()
 
 if __name__ == "__main__":
     createDB()
--- python/resetDB.py
+++ python/resetDB.py
@@ -1,20 +1,37 @@
-import os 
-from python.createDB import createDB
+import sqlite3
+import os
 
 def resetDB():
-    baseDir = os.path.dirname(__file__) 
+    baseDir = os.path.dirname(__file__)
     dbPath = os.path.join(baseDir, "..", "treningDB.db")
 
-    print("\n nullstiller databasen")
+    print("Nullstiller databasen")
 
-    if os.path.exists(dbPath):
-        os.remove(dbPath)
-        print("\n sletter eksisterende database")
-    else: 
-        print("\n ingen eksisterende database")
+    connection = sqlite3.connect(dbPath)
+    cursor = connection.cursor()
 
-    createDB()
-    print("\n ny database er opprettet")
+    try:
+        cursor.execute("PRAGMA foreign_keys = OFF")
+
+        cursor.execute("""
+            SELECT name 
+            FROM sqlite_master 
+            WHERE type='table'
+            AND name NOT LIKE 'sqlite_%'
+        """)
+
+        for table in cursor.fetchall():
+            cursor.execute(f"DELETE FROM {table[0]}")
+
+        cursor.execute("PRAGMA foreign_keys = ON")
+
+        connection.commit()
+
+        print("Databasen er nå tom")
+
+    finally:
+        connection.close()
+
 
 if __name__ == "__main__":
     resetDB()
